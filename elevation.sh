e657ff1c0d992c76524776f04dd0c27da1080508
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-03-26 22:02:41 -0700

    Switching to a single contours layer (zoom 12)

--- elevation.sh
+++ elevation.sh
@@ -1,9 +1,7 @@
 echo "Clearing out existing data..."
 rm elevation.mbtiles
-psql underfoot -c "drop table contours8"
-psql underfoot -c "drop table contours10"
+psql underfoot -c "drop table contours12"
 find elevation-tiles/ -type f -name '*.merge*' -delete
 
 echo "Make the contours and load them into PostGIS (you can probably ignore the errors)..."
-node elevation.js 8
-node elevation.js 10
+node elevation.js 12
