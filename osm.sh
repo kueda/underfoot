59dc21b239a60ed96c49849bb3d7608d6b5da3fd
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-09-03 22:33:45 -0700

    Updated osm script to download multiple states if necessar

--- osm.sh
+++ osm.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo ""
-echo "Downloading an export of recent California OSM data (thanks, Geofabrik!)"
-if ! [ -e california-latest.osm.pbf ]
-then
-  curl -o california-latest.osm.pbf http://download.geofabrik.de/north-america/us/california-latest.osm.pbf
-fi
-
 # Make a PostGIS database for the OSM data using the omsosis schema. Note that
 # those osmosis files should  have been installed with osmosis when the VM was
 # provisioned.
@@ -19,37 +12,34 @@ psql -d underfoot_ways < /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.s
 psql -d underfoot_ways < /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_bbox.sql
 psql -d underfoot_ways < /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_linestring.sql
 
-# Load ways from the PBF into the database
-echo ""
-echo "Loading data from the PBF into the database"
-# osmosis \
-#   --read-pbf california-latest.osm.pbf \
-#   --tf accept-ways highway=* \
-#   --tf reject-ways service=* \
-#   --tf reject-ways footway=sidewalk \
-#   --tf reject-ways highway=proposed \
-#   --tf reject-ways highway=footway \
-#   --tf reject-ways highway=pedestrian \
-#   --tf reject-ways highway=steps \
-#   --bounding-box left=-123 top=38 right=-122 bottom=37 \
-#   --write-pgsql database="underfoot_ways" user="underfoot" password="underfoot"
-
-osmosis \
-  --read-pbf california-latest.osm.pbf \
-  --tf accept-ways highway=* \
-  --tf reject-ways service=* \
-  --tf reject-ways footway=sidewalk \
-  --tf reject-ways highway=proposed \
-  --tf reject-ways highway=footway \
-  --tf reject-ways highway=pedestrian \
-  --tf reject-ways highway=steps \
-  --write-pgsql database="underfoot_ways" user="underfoot" password="underfoot"
+# for state in arizona california # if you want to make an osm export for multiple states
+for state in california
+do
+  echo ""
+  echo "Downloading an export of recent $state OSM data (thanks, Geofabrik!)"
+  if ! [ -e $state-latest.osm.pbf ]
+  then
+    curl -o $state-latest.osm.pbf http://download.geofabrik.de/north-america/us/$state-latest.osm.pbf
+  fi
+  # Load ways from the PBF into the database
+  echo ""
+  echo "Loading data from the PBF into the database"
+  osmosis \
+    --read-pbf $state-latest.osm.pbf \
+    --tf accept-ways highway=* \
+    --tf reject-ways service=* \
+    --tf reject-ways footway=sidewalk \
+    --tf reject-ways highway=proposed \
+    --tf reject-ways highway=footway \
+    --tf reject-ways highway=pedestrian \
+    --tf reject-ways highway=steps \
+    --write-pgsql database="underfoot_ways" user="underfoot" password="underfoot"
+done
 
 # Create a table for ways with just names and highway tags
 echo ""
 echo "Creating a table for ways with just names and highway tags"
 psql underfoot_ways -c "CREATE TABLE underfoot_ways AS SELECT id, version, tags -> 'name' AS name, tags -> 'highway' AS highway, linestring FROM ways"
-
 
 # Export ways into the MBTiles using different zoom levels for different types
 echo ""
