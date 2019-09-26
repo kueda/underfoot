28dab945ce663ed0e7e115807ef763c1165b7aff
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2019-09-25 21:52:23 -0700

    Made osm.sh write to the local dir

--- osm.sh
+++ osm.sh
@@ -44,15 +44,16 @@ psql underfoot_ways -c "CREATE TABLE underfoot_ways AS SELECT id, version, tags 
 # Export ways into the MBTiles using different zoom levels for different types
 echo ""
 echo "Exporting into MBTiles"
+rm ./underfoot_ways.mbtiles
 ./node_modules/tl/bin/tl.js copy -i underfoot_ways.json -z 3 -Z 13 \
   "postgis://underfoot:underfoot@localhost:5432/underfoot_ways?table=underfoot_ways&query=(SELECT%20*%20from%20underfoot_ways%20WHERE%20highway%20in%20('motorway'))%20AS%20foo" \
-  mbtiles:///home/underfoot/underfoot_ways.mbtiles
+  mbtiles://./underfoot_ways.mbtiles
 ./node_modules/tl/bin/tl.js copy -i underfoot_ways.json -z 7 -Z 13 \
   "postgis://underfoot:underfoot@localhost:5432/underfoot_ways?table=underfoot_ways&query=(SELECT%20*%20from%20underfoot_ways%20WHERE%20highway%20in%20('motorway','primary','trunk'))%20AS%20foo" \
-  mbtiles:///home/underfoot/underfoot_ways.mbtiles
+  mbtiles://./underfoot_ways.mbtiles
 ./node_modules/tl/bin/tl.js copy -i underfoot_ways.json -z 11 -Z 13 \
   "postgis://underfoot:underfoot@localhost:5432/underfoot_ways?table=underfoot_ways&query=(SELECT%20*%20from%20underfoot_ways%20WHERE%20highway%20in%20('motorway','primary','trunk','secondary','tertiary','motorway_link'))%20AS%20foo" \
-  mbtiles:///home/underfoot/underfoot_ways.mbtiles
+  mbtiles://./underfoot_ways.mbtiles
 ./node_modules/tl/bin/tl.js copy -i underfoot_ways.json -z 13 -Z 13 \
   "postgis://underfoot:underfoot@localhost:5432/underfoot_ways?table=underfoot_ways" \
-  mbtiles:///home/underfoot/underfoot_ways.mbtiles
+  mbtiles://./underfoot_ways.mbtiles
