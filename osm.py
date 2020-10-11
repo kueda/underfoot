1484b408ba0fabf9c16c66ad1b1a6a6a265a3dd4
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-10-11 14:22:35 -0700

    Separated OSM make_ways into separate methods

--- osm.py
+++ osm.py
@@ -9,38 +9,35 @@ DB_USER = "underfoot"
 DB_PASSWORD = "underfoot"
 TABLE_NAME = "underfoot_ways"
 
-def make_ways(pbf_url, clean=False, bbox=None, path="./ways.mbtiles"):
-  r"""Make an MBTiles files for OSM ways data given an OSM PBF export URL
+def database_connection(recreate=False):
+  if recreate:
+    util.call_cmd(["dropdb", "--if-exists", DBNAME], check=True)
+  # Check to see if db exists
+  try:
+    return psycopg2.connect("dbname={}".format(DBNAME))
+  except psycopg2.OperationalError:
+    osmosis_path = "/usr/share/doc/osmosis/examples"
+    if not os.path.isdir(osmosis_path):
+      osmosis_path = "/usr/local/Cellar/osmosis/0.45/libexec/script"
+      if not os.path.isdir(osmosis_path):
+        raise ValueError("Couldn't find osmosis path to osmosis files")
+    util.call_cmd(["createdb", DBNAME])
+    util.call_cmd(["psql", "-d", DBNAME, "-c", "CREATE EXTENSION postgis; CREATE EXTENSION hstore;"])
+    util.call_cmd(["psql", "-d", DBNAME, "-f", f"{osmosis_path}/pgsnapshot_schema_0.6.sql"])
+    util.call_cmd(["psql", "-d", DBNAME, "-f", f"{osmosis_path}/pgsnapshot_schema_0.6_bbox.sql"])
+    util.call_cmd(["psql", "-d", DBNAME, "-f", f"{osmosis_path}/pgsnapshot_schema_0.6_linestring.sql"])
+    return psycopg2.connect("dbname={}".format(DBNAME))
 
-  Parameters
-  ----------
-  pbf_url : str
-    The URL of a PBF export of OSM data
-  bbox : dict
-    Bounding box to import from the PBF export of the form
-    {"top": 1, "bottom": 0, "left": 0, "right": 1}
-  clean : bool
-    Force
-  """
-  # Bail if no PBF URL
-  if not pbf_url or len(pbf_url) == 0:
-    raise ValueError("You must specify a PBF URL")
-  filename = os.path.basename(pbf_url)
+def fetch_data(url, clean=False):
+  filename = os.path.basename(url)
   if os.path.isfile(filename) and not clean:
     pass
   else:
-    util.call_cmd(["curl", "-o", filename, pbf_url], check=True)
-  util.call_cmd(["dropdb", "--if-exists", DBNAME], check=True)
-  # Check to see if db exists
-  try:
-    con = psycopg2.connect("dbname={}".format(DBNAME))
-  except psycopg2.OperationalError:
-    util.call_cmd(["createdb", DBNAME])
-    util.call_cmd(["psql", "-d", DBNAME, "-c", "CREATE EXTENSION postgis; CREATE EXTENSION hstore;"])
-    util.call_cmd(["psql", "-d", DBNAME, "-f", "/usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql"])
-    util.call_cmd(["psql", "-d", DBNAME, "-f", "/usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_bbox.sql"])
-    util.call_cmd(["psql", "-d", DBNAME, "-f", "/usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_linestring.sql"])
-    con = psycopg2.connect("dbname={}".format(DBNAME))
+    util.call_cmd(["curl", "-o", filename, url], check=True)
+  return filename
+
+def load_ways_data(data_path, recreate=False, bbox=None):
+  con = database_connection(recreate=recreate)
   # Check to see if table exists and has rows
   cur1 = con.cursor()
   ways_table_missing = False
@@ -56,8 +53,15 @@ def make_ways(pbf_url, clean=False, bbox=None, path="./ways.mbtiles"):
     ways_table_missing = True
   con.close()
   if ways_table_missing:
+    util.call_cmd([
+      "osmosis",
+      "--truncate-pgsql",
+      "database={}".format(DBNAME),
+      "user={}".format(DB_USER),
+      "password={}".format(DB_PASSWORD)
+    ])
     read_args = [
-      "--read-pbf", filename,
+      "--read-pbf", data_path,
       "--tf", "accept-ways", "highway=*",
       "--tf", "reject-ways", "service=*",
       "--tf", "reject-ways", "footway=sidewalk",
@@ -84,13 +88,19 @@ def make_ways(pbf_url, clean=False, bbox=None, path="./ways.mbtiles"):
     cmd = ["osmosis"] + read_args + write_args
     # Load data from PBF into the database with osmosis
     util.call_cmd(cmd)
-  util.call_cmd(["psql", DBNAME, "-c", "DROP TABLE {}".format(TABLE_NAME)])
-  util.call_cmd([
-    "psql",
-    DBNAME,
-    "-c",
-    "CREATE TABLE {} AS SELECT id, version, tags -> 'name' AS name, tags -> 'highway' AS highway, linestring FROM ways".format(TABLE_NAME)
-  ])
+  util.run_sql(f"DROP TABLE IF EXISTS {TABLE_NAME}", dbname=DBNAME)
+  util.run_sql(f"""
+    CREATE TABLE {TABLE_NAME} AS
+    SELECT
+      id,
+      version,
+      tags -> 'name' AS name,
+      tags -> 'highway' AS highway,
+      linestring
+    FROM ways
+  """, dbname=DBNAME)
+
+def make_ways_mbtiles(path):
   # Export ways into the MBTiles using different zoom levels for different types
   if os.path.exists(path):
     os.remove(path)
@@ -137,6 +147,26 @@ def make_ways(pbf_url, clean=False, bbox=None, path="./ways.mbtiles"):
     ),
     "mbtiles://{}".format(path)
   ])
+
+def make_ways(pbf_url, clean=False, bbox=None, path="./ways.mbtiles"):
+  r"""Make an MBTiles files for OSM ways data given an OSM PBF export URL
+
+  Parameters
+  ----------
+  pbf_url : str
+    The URL of a PBF export of OSM data
+  bbox : dict
+    Bounding box to import from the PBF export of the form
+    {"top": 1, "bottom": 0, "left": 0, "right": 1}
+  clean : bool
+    Force
+  """
+  # Bail if no PBF URL
+  if not pbf_url or len(pbf_url) == 0:
+    raise ValueError("You must specify a PBF URL")
+  filename = fetch_data(pbf_url, clean=clean)
+  load_ways_data(filename, recreate=True, bbox=bbox)
+  make_ways_mbtiles(path)
   return path
 
 if __name__ == "__main__":
