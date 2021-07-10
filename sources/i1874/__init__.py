d7f1389f3858424a3c327ce5a93af4565d111c62
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2021-07-10 11:53:30 -0700

    Forgot to specify that Yosemite needs to use unzip

--- sources/i1874/__init__.py
+++ sources/i1874/__init__.py
@@ -6,6 +6,7 @@ def run():
     util.process_usgs_source(
         base_path=os.path.realpath(__file__),
         url="https://pubs.usgs.gov/imap/i1874/yosenp.e00.zip",
+        use_unzip=True,
         e00_path="yosenp.e00",
         skip_polygonize_arcs=True,
         srs="+proj=utm +zone=11 +datum=NAD27 +units=m +no_defs",
