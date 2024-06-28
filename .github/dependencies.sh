8ea9817f4f0abf5c82bdf01fec4752f4e911b68b
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-06-27 21:42:44 -0700

    Trying to uninstall numpy first

--- .github/dependencies.sh
+++ .github/dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo add-apt-repository ppa:ubuntugis/ppa
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install -y \
@@ -14,9 +14,12 @@ sudo apt-get install -y \
   python-is-python3 \
   python3 \
   python3-dev \
-  python3-gdal \
   python3-pip \
   sqlite3 \
   unzip \
   virtualenv \
   zip
+
+pip uninstall numpy
+pip install "numpy<2.0"
+pip install GDAL==$(gdal-config --version) --global-option=build_ext --global-option="-I/usr/include/gdal"
